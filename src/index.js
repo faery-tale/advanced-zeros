986159925970cd670d989c52a8109ede66bdf7e8
Ekaterina Denisenko <lira-gde-to@rambler.ru>
2019-03-20 20:41:28 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  var answer = 0;
+  while (number>0){
+     number = ~~(number/5);
+     answer+=number;
+  }
+  return answer;
 }
